0f06158de9a09704f32e9d1445934cbe9a07b537
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-02-28 12:44:54 -0500

    cxx: constructor and destructor for matrix

--- cxx/linear_algebra.h
+++ cxx/linear_algebra.h
@@ -28,6 +28,9 @@ void vector_add(const vector& x, const vector& y, vector& z);
 
 struct matrix
 {
+  matrix(int n_rows, int n_cols);
+  ~matrix();
+
   double* data;
   int n_rows;
   int n_cols;
@@ -35,8 +38,6 @@ struct matrix
 
 #define MAT(A, i, j) (A).data[(i) * (A).n_cols + (j)]
 
-void matrix_construct(matrix& A, int n_rows, int n_cols);
-void matrix_destruct(matrix& A);
 void matrix_print(const matrix& A);
 
 void matrix_vector_mul(const matrix& A, const vector& x, vector& y);
