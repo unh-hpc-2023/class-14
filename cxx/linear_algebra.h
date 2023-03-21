9ef3de72c1507ecc2f486680b18b04407d0d19b1
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-03-20 22:08:24 -0400

    class14: provide Wtime() function

--- cxx/linear_algebra.h
+++ cxx/linear_algebra.h
@@ -18,4 +18,17 @@ using matrix = xt::xtensor<double, 2>;
 
 vector dot(const matrix& A, const vector& x);
 
+// ----------------------------------------------------------------------
+// other useful stuff
+
+#include <stdlib.h>
+#include <sys/time.h>
+
+static inline double Wtime(void)
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec + tv.tv_usec / 1e6;
+}
+
 #endif
